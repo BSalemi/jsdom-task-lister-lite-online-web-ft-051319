ba05c63be6bdbeb000a157dec10c254b92ed0bcc
Brian Salemi <briandsalemi@gmail.com>
2020-02-03 00:15:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,21 +3,3 @@
 // console.log(taskForm);
 // console.log(taskList);
 // console.log(taskDescription);
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
- 
-  let taskForm = document.getElementById("create-task-form");
-  let taskList = document.getElementById("tasks");
-  let taskDescription = document.getElementById("new-task-description");
-      
-      taskForm.addEventListener("submit", function(e)
-      {
-        e.preventDefault();
-        let newTask = taskDescription.value ;
-        taskList.innerHTML += `<ul>${newTask}</ul>`; 
-        taskDescription.value = ""
-       
-})
-});
